b7ec5f37d0a07cf8e723093e941b3c7d7b851bf1
kees <72143341+keeswie@users.noreply.github.com>
2022-03-20 19:02:40 +0100

    das

---
+++ headwind/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Sidebar from "./sidebar";
+function App() {
+  return (
+    <div className="flex">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
